cedd32a9f34a9225758f2e617cc62ebda6c84538
William <william781201@gmail.com>
2015-01-20 18:01:49 -0800

    Signed-off-by:William <william781201@gmail.com>

--- application/config/routes.php
+++ application/config/routes.php
@@ -39,12 +39,12 @@
 */
 
 $route['default_controller'] = "welcome";
-$route['sleep'] = 'first/zzz';
-$route['lock/(:any)'] = 'welcome/shucks';
-$route['show/(:num)'] = 'first/fimme/$1';
+$route['sleep'] = "first/zzz";
+$route['lock/(:any)/(:any)'] = "welcome/shucks";
+$route['show/(:num)'] = "first/fimme/$1";
 $route['dunno'] = "guess";
-$route['([a-z]{4})/bingo'] = 'bingo';
-$route['comp([0 - 9]{4})/(:any:)'] = 'bingo/wisdom';
+$route['([a-z]{4})/bingo'] = "bingo";
+$route['comp(\d+)/(.*)'] = "bingo/wisdom";
 $route['404_override'] = '';
 
 
